a5321fe17033177c19312146d12d1f3f30291cdd
currysoda <currysoda1598@gmail.com>
2025-10-30 02:52:38 +0900

    오타수정

--- src/tips/SubSetBitMasking.java
+++ src/tips/SubSetBitMasking.java
@@ -1,7 +1,7 @@
 package tips;
 
 // 부분집합의 경우의 수 찾기
-public class SubSetkBitMasking {
+public class SubSetBitMasking {
 	
 	public static void main(String[] args) {
 		
@@ -11,14 +11,17 @@ public class SubSetkBitMasking {
 		
 		// 부분 집합의 총 갯수는 2^n 개 이다.
 		// i = 부분 집합의 총 갯수
-		for (int i = 0; i < (1 << n); i++) {
+		for (int i = 0; i < (1 << n); i++)
+		{
 			int           temp = i;
 			StringBuilder sb   = new StringBuilder();
 			// 비트 마스킹 방법으로 부분 집합 경우의 수는 부분 집합의 경우의 수와 1과 비트 비교 연산으로 계산한다.
 			// j = 집합 원소 갯수
-			for (int j = 0; j < n; j++) {
+			for (int j = 0; j < n; j++)
+			{
 				// 2진수의 끝자리와 1과 AND 비트 비교해서 1이면 포함 0이면 포함하지 않는다.
-				if ((temp & 1) == 1) {
+				if ((temp & 1) == 1)
+				{
 					sb.append(arr[j]).append(" ");
 				}
 				// 계속 끝자리 비교를 위해 temp를 오른쪽으로 한칸 밀어준다.
